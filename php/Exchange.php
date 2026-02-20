93684eef0692c957e4eff0367eabc3517545a983
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-02-20 20:26:36 +0400

    test: add keysort base test (#27782)
    * test: add keysort base test
    
    * goskip
    
    * keysort fix

--- php/Exchange.php
+++ php/Exchange.php
@@ -848,7 +848,7 @@ class Exchange {
 
     public static function keysort($array) {
         $result = $array;
-        ksort($result);
+        ksort($result, SORT_STRING);
         return $result;
     }
 
