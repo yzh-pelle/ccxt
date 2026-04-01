e4c624e252e38399ed983d650dde8cc5d316591c
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-04-01 11:50:40 +0400

    test: base58ToBinary (#28271)
    * test: base58ToBinary
    
    * go skip

--- ts/src/test/base/tests.init.ts
+++ ts/src/test/base/tests.init.ts
@@ -50,6 +50,7 @@ import testUrlencodeNested from './test.urlencodeNested.js';
 import testStringToBase16 from './test.stringToBase16.js';
 import testCapitalize from './test.capitalize.js';
 import testConstants from './test.constants.js';
+import testBase58ToBinary from './test.base58ToBinary.js';
 import testIsJsonEncodedObject from './test.isJsonEncodedObject.js';
 import testEncodeDecode from './test.encodeDecode.js';
 
@@ -70,6 +71,7 @@ async function baseTestsInit () {
     testPrecise ();
     testSafeMethods ();
     testSafeTicker ();
+    testBase58ToBinary ();
     testToArray ();
     testJson ();
     testSortBy ();
